7a0a00b5fe06b863033a3ad23fc5cc9d8332a81d
Muhib_ur_Rehman <mrehman@nisum.com>
2021-07-27 19:24:58 +0500

    composition example

---
+++ src/com/company/composition/book.java
@@ -0,0 +1,12 @@
+package com.company.composition;
+
+public class book {
+
+    public String name;
+    public int price;
+
+    public book(String name, int price){
+        this.name=name;
+        this.price=price;
+    }
+}
---
+++ src/com/company/composition/library.java
@@ -0,0 +1,12 @@
+package com.company.composition;
+
+import java.util.ArrayList;
+
+public class library {
+
+    public ArrayList<book> list;
+
+    public library (ArrayList<book> list){
+        this.list=list;
+    }
+}
