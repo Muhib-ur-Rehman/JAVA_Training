f447aa3c4e5f2af04005d2366c4210534164de6b
Muhib_ur_Rehman <mrehman@nisum.com>
2021-07-27 14:06:13 +0500

    encapsulation work 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        student s1 = new student("Ali",2,"Male",12);
+        System.out.println(s1.toString());
     }
 }
