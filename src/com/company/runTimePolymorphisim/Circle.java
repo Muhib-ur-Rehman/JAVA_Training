9ba92eed31baf72497c6650db5492c3abcff3ebe
Muhib_ur_Rehman <mrehman@nisum.com>
2021-07-28 18:27:09 +0500

    Runtime polymorphissim

---
+++ src/com/company/runTimePolymorphisim/Circle.java
@@ -0,0 +1,7 @@
+package com.company.runTimePolymorphisim;
+
+public class Circle extends Shapes1{
+    public float calculateArea(Float constant, int radius){
+        return  constant*radius*radius;
+    }
+}
---
+++ src/com/company/runTimePolymorphisim/Triangle.java
@@ -0,0 +1,7 @@
+package com.company.runTimePolymorphisim;
+
+public class Triangle extends Shapes1{
+    public float calculateArea(Float constant , int base , int height){
+        return  constant*base*height;
+    }
+}
