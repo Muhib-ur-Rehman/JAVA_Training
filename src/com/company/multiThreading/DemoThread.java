c9fad1d55eefc8eba21f8221ee79e47608494248
Muhib_ur_Rehman <mrehman@nisum.com>
2021-08-04 20:57:28 +0500

    Multi threading

--- src/com/company/multiThreading/DemoThread.java
+++ src/com/company/multiThreading/DemoThread.java
@@ -4,10 +4,12 @@ public class DemoThread implements Runnable {
     @Override
     public void run() {
         try {
-            Thread.sleep(1000);
+            for (int i =0; i <5 ; i++){
+                System.out.println(Thread.currentThread().getName()+" : "+i);
+                Thread.sleep(1000);
+            }
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        System.out.println("Thread run successfully");
     }
 }
--- src/com/company/multiThreading/Main.java
+++ src/com/company/multiThreading/Main.java
@@ -1,7 +1,7 @@
 package com.company.multiThreading;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
         DemoThread demoThread1 = new DemoThread();
         Thread thread1 = new Thread(demoThread1);
@@ -9,5 +9,16 @@ public class Main {
         DemoThread demoThread2 = new DemoThread();
         Thread thread2 = new Thread(demoThread2);
         thread2.start();
+        thread1.join();
+        thread2.join();
+
+        try {
+            for (int i =0; i <5 ; i++){
+                System.out.println(Thread.currentThread().getName()+" : "+i);
+                Thread.sleep(1000);
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
